7bf14d1e6aedc1137b764bb6fc343df8f908a710
Sharad Chand <pepsighan@sharadchand.com>
2022-01-27 13:37:43 +0545

    Show mint fee after calc

--- web/components/MintFee.tsx
+++ web/components/MintFee.tsx
@@ -1,21 +1,23 @@
 import { Grid, Paper, Typography } from "@mui/material";
 import { useFormContext, useWatch } from "react-hook-form";
-import { formatAmount } from "utils/metis";
+import { calculateMintFee, calculateWei, formatWeiAmount } from "utils/metis";
 
 export default function MintFee() {
   const { control } = useFormContext();
-  const amount = useWatch({ control, name: "amount" });
+  const amountString = useWatch({ control, name: "amount" });
+  const amount = amountString ? calculateWei(amountString) : null;
+  const mintFee = amount ? calculateMintFee(amount) : null;
+  const total = mintFee ? amount!.plus(mintFee) : null;
 
   return (
     <Paper variant="outlined" sx={{ p: 2, bgcolor: "grey.100" }}>
       <Grid container>
-        <Grid item xs={8}>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            sx={{ textAlign: "right" }}
-          >
+        <Grid item xs={8} sx={{ textAlign: "right" }}>
+          <Typography variant="body2" color="textSecondary">
             Mint Fee
+          </Typography>
+          <Typography variant="caption" color="textSecondary">
+            (5% or 1 Metis whichever is lower)
           </Typography>
         </Grid>
         <Grid item xs={4}>
@@ -23,7 +25,7 @@ export default function MintFee() {
             variant="body2"
             sx={{ textAlign: "right", fontWeight: "medium" }}
           >
-            {formatAmount(amount || "0")}
+            {formatWeiAmount(mintFee?.toString() ?? "0")}
           </Typography>
         </Grid>
 
@@ -41,7 +43,7 @@ export default function MintFee() {
             variant="body2"
             sx={{ textAlign: "right", fontWeight: "medium" }}
           >
-            {formatAmount(amount || "0")}
+            {formatWeiAmount(total?.toString() ?? "0")}
           </Typography>
         </Grid>
       </Grid>
