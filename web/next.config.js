8786007f2b040254a9ae1199bd2ab0df71067d66
Sharad Chand <pepsighan@sharadchand.com>
2022-01-28 18:13:39 +0545

    Make permanent redirect

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  redirects: async () => [
+    {
+      source: "/:path*",
+      has: [{ type: "host", value: "www.giftnft.cards" }],
+      destination: "https://giftnft.cards/:path*",
+      permanent: true,
+    },
+  ],
+};
