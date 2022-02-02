b3208488457458d8d7c3713cee3f8fdef751bb97
Sharad Chand <pepsighan@sharadchand.com>
2022-02-02 08:23:04 +0545

    Allow adding network

--- web/components/InvalidChain.tsx
+++ web/components/InvalidChain.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Typography } from "@mui/material";
+import { Button, Container, Stack, Typography } from "@mui/material";
 import { MdSwitchRight } from "react-icons/md";
 import { useCallback } from "react";
 import { getMetamask } from "utils/metamask";
@@ -6,8 +6,43 @@ import { useAccount } from "store/account";
 import config from "utils/config";
 
 export default function InvalidChain() {
+  const network = useAccount(useCallback((state) => state.network, []));
+
+  const onAddNetwork = useCallback(async () => {
+    const chainId =
+      network === "mainnet" ? config.MAINNET_CHAIN_ID : config.TESTNET_CHAIN_ID;
+
+    const metamask = await getMetamask();
+    await metamask.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          chainId: "0x" + chainId.toString(16),
+          chainName:
+            network === "mainnet"
+              ? "Metis Andromeda Mainnet"
+              : "Metis Stardust Testnet",
+          nativeCurrency: {
+            name: "Metis",
+            symbol: "METIS",
+            decimals: 18,
+          },
+          rpcUrls: [
+            network === "mainnet"
+              ? "https://andromeda.metis.io/?owner=1088"
+              : "https://stardust.metis.io/?owner=588",
+          ],
+          blockExplorerUrls: [
+            network === "mainnet"
+              ? "https://andromeda-explorer.metis.io/"
+              : "https://stardust-explorer.metis.io/",
+          ],
+        },
+      ],
+    });
+  }, [network]);
+
   const onSwitch = useCallback(async () => {
-    const network = useAccount.getState().network;
     const chainId =
       network === "mainnet" ? config.MAINNET_CHAIN_ID : config.TESTNET_CHAIN_ID;
 
@@ -16,7 +51,7 @@ export default function InvalidChain() {
       method: "wallet_switchEthereumChain",
       params: [{ chainId: "0x" + chainId.toString(16) }],
     });
-  }, []);
+  }, [network]);
 
   return (
     <Container
@@ -29,10 +64,20 @@ export default function InvalidChain() {
       }}
     >
       <MdSwitchRight size={80} />
-      <Typography>You are not connected to Metis Blockchain.</Typography>
-      <Button variant="contained" sx={{ mt: 4 }} onClick={onSwitch}>
-        Switch Network
-      </Button>
+      <Typography>
+        You are not connected to Metis{" "}
+        {network === "mainnet" ? "Andromeda Mainnet" : "Stardust Testnet"}.
+      </Typography>
+
+      <Stack direction="row" spacing={2} sx={{ mt: 4 }}>
+        <Button variant="outlined" onClick={onAddNetwork}>
+          Add Metis{" "}
+          {network === "mainnet" ? "Andromeda Mainnet" : "Stardust Testnet"}
+        </Button>
+        <Button variant="contained" onClick={onSwitch}>
+          Switch Network
+        </Button>
+      </Stack>
     </Container>
   );
 }
