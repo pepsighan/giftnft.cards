3fcfc77647a1fdc1e3f01a8c65914d281bd2b684
Sharad Chand <pepsighan@sharadchand.com>
2022-04-28 08:14:19 +0530

    Extract the backend specific envs in the backend only

--- web/utils/config.ts
+++ web/utils/config.ts
@@ -1,4 +1,4 @@
-import contractAbi from "utils/contractAbi";
+import contractAbi from 'utils/contractAbi';
 
 const config = {
   TESTNET_CHAIN_ID: parseInt(process.env.NEXT_PUBLIC_TESTNET_CHAIN_ID!),
@@ -6,10 +6,6 @@ const config = {
   MAINNET_CHAIN_ID: parseInt(process.env.NEXT_PUBLIC_MAINNET_CHAIN_ID!),
   MAINNET_CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_MAINNET_CONTRACT_ADDRESS!,
   CONTRACT_ABI: contractAbi,
-  TESTNET_HTTP_RPC_ENDPOINT: process.env.TESTNET_HTTP_RPC_ENDPOINT,
-  TESTNET_ADMIN_PRIVATE_KEY: process.env.TESTNET_ADMIN_PRIVATE_KEY,
-  MAINNET_HTTP_RPC_ENDPOINT: process.env.MAINNET_HTTP_RPC_ENDPOINT,
-  MAINNET_ADMIN_PRIVATE_KEY: process.env.MAINNET_ADMIN_PRIVATE_KEY,
 };
 
 if (
@@ -18,31 +14,7 @@ if (
   !config.TESTNET_CHAIN_ID ||
   !config.MAINNET_CHAIN_ID
 ) {
-  throw new Error("environment is not configured");
-}
-
-/**
- * Gets the network configuration to connect and execute transactions.
- */
-export function getMetisNetworkConfig(environment: string) {
-  switch (environment) {
-    case "mainnet":
-      return {
-        endpoint: config.MAINNET_HTTP_RPC_ENDPOINT,
-        privateKey: config.MAINNET_ADMIN_PRIVATE_KEY,
-        chainId: config.MAINNET_CHAIN_ID,
-        contractAddress: config.MAINNET_CONTRACT_ADDRESS,
-      };
-    case "testnet":
-      return {
-        endpoint: config.TESTNET_HTTP_RPC_ENDPOINT,
-        privateKey: config.TESTNET_ADMIN_PRIVATE_KEY,
-        chainId: config.TESTNET_CHAIN_ID,
-        contractAddress: config.TESTNET_CONTRACT_ADDRESS,
-      };
-    default:
-      throw new Error("unreachable");
-  }
+  throw new Error('environment is not configured');
 }
 
 export default config;
